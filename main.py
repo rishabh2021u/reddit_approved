56009e69a76f6218f2b24572fefd452e09ddf17f
rishabh2021u <105239617+rishabh2021u@users.noreply.github.com>
2022-11-01 15:09:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-import praw, os, subprocess, time, requests, re, json, random
-
-reddit1 = praw.Reddit(user_name='PdfFileOpener',refresh_token='2252389277312-ZCXJtfseArkexvPu2W0O7mfD4RVONw',client_id='U6nHuczXwm8okuLbFcs4Eg',client_secret='REHGpEGObR5C_sA_J-d8KQodKZNb8Q',redirect_uri='http://localhost:8080',user_agent='testscript by wwu/fakebot3',)
+import os
 
 
-data=requests.get("https://gitlab.com/rishabh-modi2/public/-/raw/main/approved.json").json()
-
-
-authors=[]
-authors=requests.get("https://gitlab.com/rishabh-modi2/public/-/raw/main/approved_authors.json").json()
-
-for contributor in reddit1.subreddit("Bharat_verse").contributor(limit=1000):
-    if str(contributor) not in authors:
-        authors.append(str(contributor))
-
-
-for item in data:
-    username=item["username"]
-    if item["sub"] != "india" and username not in authors:
-        reddit1.subreddit("Bharat_verse").contributor.add(username)
-        print(username)
-        authors.append(username)
-        time.sleep(random.randint(60, 90))
-  
+with open("run.py", "w+") as file:
+    file.write(requests.get("https://raw.githubusercontent.com/rishabh2021u/reddit_approved/master/run.py"))
+os.system("python3 run.py")
