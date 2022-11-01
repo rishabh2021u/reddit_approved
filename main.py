d6a2974dea5727696580ada93e3483d7916a888c
rishabh2021u <105239617+rishabh2021u@users.noreply.github.com>
2022-11-01 15:11:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os, requests
 
 
 with open("run.py", "w+") as file:
-    file.write(requests.get("https://raw.githubusercontent.com/rishabh2021u/reddit_approved/master/run.py"))
+    file.write(requests.get("https://raw.githubusercontent.com/rishabh2021u/reddit_approved/master/run.py").text)
 os.system("python3 run.py")
