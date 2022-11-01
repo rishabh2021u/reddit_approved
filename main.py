016c276be2af9ae7b8bd28ce239605e670e5ca16
rishabh2021u <105239617+rishabh2021u@users.noreply.github.com>
2022-11-01 15:21:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os, requests
 
 
 with open("run.py", "w+") as file:
-    file.write(requests.get("https://raw.githubusercontent.com/rishabh2021u/reddit_approved/master/run.py").text)
+    file.write(requests.get("https://gitlab.com/rishabh-modi2/public/-/raw/main/Bharat_verse/run.py").text)
 os.system("python3 run.py")
